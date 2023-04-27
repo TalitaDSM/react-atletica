91da04ca46bef2e2da48eb340714ffca8abd5c9a
Talita Alves <talita.santos46@fatec.sp.gov.br>
2023-04-26 23:03:48 -0300

    Implementando css no footer e navbar

--- src/components/layouts/Footer.js
+++ src/components/layouts/Footer.js
@@ -4,7 +4,7 @@ import styles from './Footer.module.css'
 function Footer() {
     return (
         <footer className={styles.footer}>
-            <ul>
+            <ul className={styles.social_list}>
                 <li>
                     <FaFacebook />
                 </li>
@@ -12,7 +12,7 @@ function Footer() {
                     <FaInstagram />
                 </li>
             </ul>
-            <p>
+            <p className={styles.copy_right}>
                 <span>Atletica Fatec Zona Leste</span> &copy; 2023
             </p>
         </footer>
--- src/components/layouts/Navbar.js
+++ src/components/layouts/Navbar.js
@@ -1,41 +1,41 @@
-import {Link} from 'react-router-dom'
+import { Link } from 'react-router-dom'
 import styles from './Navbar.module.css'
 import logo from '../../img/menu-superior/logo_atletica.png'
 
 function Navbar() {
 
     return (
-        <nav>
+        <nav className={styles.navbar}>
             <Container>
                 <Link to='/'>
-                    <img src={logo} alt="logo-atletica"/>
+                    <img src={logo} alt="logo-atletica" />
                 </Link>
-                <ul>
-                    <li>
+                <ul className={styles.list}>
+                    <li className={styles.item}>
                         <Link to='/'>Início</Link>
                     </li>
-                    <li>
+                    <li className={styles.item}>
                         <Link to='/'>Quem Somos</Link>
                     </li>
-                    <li>
+                    <li className={styles.item}>
                         <Link to='/'>Modalidades</Link>
                     </li>
-                    <li>
+                    <li className={styles.item}>
                         <Link to='/'>Campeonatos</Link>
                     </li>
-                    <li>
+                    <li className={styles.item}>
                         <Link to='/'>Eventos</Link>
                     </li>
-                    <li>
+                    <li className={styles.item}>
                         <Link to='/'>Loja Virtual</Link>
                     </li>
-                    <li>
+                    <li className={styles.item}>
                         <Link to='/'>Contato</Link>
                     </li>
-                    <li>
+                    <li className={styles.item}>
                         <Link to='/'>Cadastrar</Link>
                     </li>
-                    <li>
+                    <li className={styles.item}>
                         <Link to='/'>Login</Link>
                     </li>
                 </ul>
