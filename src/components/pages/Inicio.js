7a5255d6aa1e0ef2d30eb1190bdc2cd13b115c8d
TalitaDSM <talita.santos46@fatec.sp.gov.br>
2023-05-10 09:49:27 -0300

    Atualizando component de Inicio e css

--- src/components/pages/Inicio.js
+++ src/components/pages/Inicio.js
@@ -1,7 +1,91 @@
 import React from "react";      
 
 function Inicio () {
-    return <h1>Inicio</h1>
+    return 
+    <main id="conteudo">
+        <section class="banner">
+            <div class="banner__imagem indexImagem"> </div>
+            <h1 class="banner__titulo">ATLÉTICA FATEC ZONA LESTE</h1>
+        </section>
+        <div vw class="enabled">
+            <div vw-access-button class="active"></div>
+            <div vw-plugin-wrapper>
+              <div class="vw-plugin-top-wrapper"></div>
+            </div>
+        </div>
+        <script src="https://vlibras.gov.br/app/vlibras-plugin.js"></script>
+        <script>
+            new window.VLibras.Widget('https://vlibras.gov.br/app');
+        </script>
+        <section class="mural">
+            <h2 class="mural__titulo">Fique por dentro do que acontece na Atlética Fatec ZL</h2>
+            <div class="noticias">
+                <div class="noticia">
+                    <img class="noticia__imagem" src="./assets/img/mural/modalidades_mural.PNG " alt="Imagem contendo a equipe de futsal feminino da Fatec">
+                    <div class="noticia__conteudo">
+                        <h3 class="noticia__titulo">Modalidades Esportivas</h3>
+                        <p class="noticia__descricao">Conheça e participe das modalidades esportivas da nossa Atlética.</p>
+                    </div>
+                </div>
+                <div class="noticia">
+                    <img class="noticia__imagem" src="assets\img\mural\eventos_multirao_atletica.jpg" alt="Imagem contendo a frase mutirão fatec zl juntamente com a data de vinte e um de novembro">
+                    <div class="noticia__conteudo">
+                        <h3 class="noticia__titulo">Multirão de Limpeza</h3>
+                        <p class="noticia__descricao">Participe do Multirão de Limpeza 2022.</p>
+                    </div>
+                </div>
+                <div class="noticia">
+                    <img class="noticia__imagem" src="assets\img\mural\college_vermelha.webp" alt="Imagem contendo a frase mutirão fatec zl juntamente com a data de vinte e um de novembro">
+                    <div class="noticia__conteudo">
+                        <h3 class="noticia__titulo">Lojinha da Atlética</h3>
+                        <p class="noticia__descricao">Conheça os novos produtos da lojinha da Atlética.</p>
+                    </div>
+                </div>
+                <div class="noticia">
+                    <img class="noticia__imagem" src="./assets/img/mural/modalidades_mural.PNG " alt="Imagem contendo a equipe de futsal feminino da Fatec">
+                    <div class="noticia__conteudo">
+                        <h3 class="noticia__titulo">Modalidades Esportivas</h3>
+                        <p class="noticia__descricao">Conheça e participe das modalidades esportivas da nossa Atlética.</p>
+                    </div>
+                </div>
+                <div class="noticia">
+                    <img class="noticia__imagem" src="assets\img\mural\eventos_multirao_atletica.jpg" alt="Imagem contendo a frase mutirão fatec zl juntamente com a data de vinte e um de novembro">
+                    <div class="noticia__conteudo">
+                        <h3 class="noticia__titulo">Multirão de Limpeza</h3>
+                        <p class="noticia__descricao">Participe do Multirão de Limpeza 2022.</p>
+                    </div>
+                </div>
+                <div class="noticia">
+                    <img class="noticia__imagem" src="assets\img\mural\college_vermelha.webp" alt="Imagem contendo a frase mutirão fatec zl juntamente com a data de vinte e um de novembro">
+                    <div class="noticia__conteudo">
+                        <h3 class="noticia__titulo">Lojinha da Atlética</h3>
+                        <p class="noticia__descricao">Conheça os novos produtos da lojinha da Atlética.</p>
+                    </div>
+                </div>
+                <div class="noticia">
+                    <img class="noticia__imagem" src="./assets/img/mural/modalidades_mural.PNG " alt="Imagem contendo a equipe de futsal feminino da Fatec">
+                    <div class="noticia__conteudo">
+                        <h3 class="noticia__titulo">Modalidades Esportivas</h3>
+                        <p class="noticia__descricao">Conheça e participe das modalidades esportivas da nossa Atlética.</p>
+                    </div>
+                </div>
+                <div class="noticia">
+                    <img class="noticia__imagem" src="assets\img\mural\eventos_multirao_atletica.jpg" alt="Imagem contendo a frase mutirão fatec zl juntamente com a data de vinte e um de novembro">
+                    <div class="noticia__conteudo">
+                        <h3 class="noticia__titulo">Multirão de Limpeza</h3>
+                        <p class="noticia__descricao">Participe do Multirão de Limpeza 2022.</p>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <section class="mapa">
+            <h3 class="mapa__titulo">LOCALIZAÇÃO</h3>
+            <p class="mapa__descricao">A FATEC ZL está localizada no coração da Zona Leste de São Paulo.</p>
+            <div class="mapa__iframe">
+               <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3658.3097954317063!2d-46.47770326835123!3d-23.521357523908254!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94ce61270808f47f%3A0xddc2deca4778eb69!2sFATEC%20Zona%20Leste%20%2F%20Vila%20Uni%C3%A3o!5e0!3m2!1spt-BR!2sbr!4v1637589573056!5m2!1spt-BR!2sbr" width="90%" height="350" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
+            </div>
+        </section>
+    </main>
 }
 
 export default Inicio
