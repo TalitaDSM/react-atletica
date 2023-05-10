cd5a26cd27f9a4907e73163dde0c424d8db12c0b
Talita Alves <talitaalves@movida.com.br>
2023-05-10 17:16:30 -0300

    Ajustando css

--- src/components/layouts/Acessibilidade.js
+++ src/components/layouts/Acessibilidade.js
@@ -1,22 +1,24 @@
-import React from "react";      
+import React from "react";
+import styles from './Acessibilidade.module.css'     
 
 function Acessibilidade () {
-    return 
+    return (
     
-    <main class="acessibilidade">
-        <ul class="acessibilidade__atalhos">
+    <main className={styles.acessibilidade}>
+        <ul className={styles.acessibilidade__atalhos}>
             <li><a href="#conteudo" accesskey="1" title="Ir diretamente para o conteúdo">Ir para o conteúdo [1]</a></li>
             <li><a href="#menu" accesskey="2" title="Ir diretamente para o menu">Ir para o menu [2]</a></li>
             <li><a href="#rodape" accesskey="3" title="Ir diretamente para o rodapé">Ir para o rodapé [3]</a></li>
         </ul>
-        <ul class="acessibilidade__links">
+        <ul classNAme={styles.acessibilidade__links}>
             <li><a href="acessibilidade.html" title="Ir para a página de Acessibilidade">ACESSIBILIDADE</a></li>
-            <li class="contraste"><a href="javascript:void(0);" id="contraste">CONTRASTE PRETO</a></li>
-            <li class="contraste"><a href="javascript:void(0);" id="sem_contraste">SEM CONTRASTE</a></li>
-            <li class="fonte"><a href="javascript:void(0);" id="aumentar">A+</a></li>
-            <li class="fonte"><a href="javascript:void(0);" id="diminuir">A-</a></li>
+            <li className={styles.contrate}><a href="javascript:void(0);" id="contraste">CONTRASTE PRETO</a></li>
+            <li className={styles.constrate}><a href="javascript:void(0);" id="sem_contraste">SEM CONTRASTE</a></li>
+            <li className={styles.fonte}><a href="javascript:void(0);" id="aumentar">A+</a></li>
+            <li className={styles.fonte}><a href="javascript:void(0);" id="diminuir">A-</a></li>
         </ul>
     </main>
+    )
 }
 
 export default Acessibilidade
