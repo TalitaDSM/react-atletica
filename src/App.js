8827704218cb007d5c43bb5f2496782f734b0604
Talita Alves <talita.santos46@fatec.sp.gov.br>
2023-05-25 22:04:34 -0300

    Subindo projeto para repositorio remoto

--- src/App.js
+++ src/App.js
@@ -1,31 +1,29 @@
 import React from 'react'
-import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 
-import Acessibilidade from './components/layouts/Acessibilidade'
+//import Acessibilidade from './components/layouts/Acessibilidade'
 import Navbar from './components/layouts/Navbar'
 import Footer from './components/layouts/Footer'
 import Container from './components/layouts/Container'
-import Cadastro from './components/pages/Cadastro'
-import Campeonatos from './components/pages/Campeonatos'
-import Contato from './components/pages/Contato'
-import Eventos from './components/pages/Eventos'
 import Inicio from './components/pages/Inicio'
-import LoginPage from './components/pages/LoginPage'
-import LojaVirtual from './components/pages/LojaVirtual'
-import Modalidades from './components/pages/Modalidades'
-import QuemSomos from './components/pages/QuemSomos'
 import AcessibilidadePage from './components/pages/AcessibilidadePage'
+import Cadastro from './components/pages/Cadastro'
+import QuemSomos from './components/pages/QuemSomos'
+import Modalidades from './components/pages/Modalidades'
+import Campeonatos from './components/pages/Campeonatos'
+import Eventos from './components/pages/Eventos'
+import LojaVirtual from './components/pages/LojaVirtual'
+import Contato from './components/pages/Contato'
+import LoginPage from './components/pages/LoginPage'
 
 
 function App() {
   return (
     <Router>
-      <Acessibilidade />
       <Navbar />
       <Switch>
         <Container customClass="min-height">
-          <Route exact path="/">
-            <Inicio />
+          <Route exact path="/" component={Inicio}>
           </Route>
           <Route exact path="/acessibilidadepage" component={AcessibilidadePage}>
           </Route>
